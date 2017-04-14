b70cd95e39eda8937bec939be63245228cc9a0e2
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2017-04-14 11:52:19 -0400

    Relax generics bounds for PaintPixelsGenerator

--- src/main/java/net/imglib2/atlas/control/brush/PaintPixelsGenerator.java
+++ src/main/java/net/imglib2/atlas/control/brush/PaintPixelsGenerator.java
@@ -5,9 +5,8 @@ import java.util.Iterator;
 import net.imglib2.Localizable;
 import net.imglib2.RandomAccessible;
 import net.imglib2.RealLocalizable;
-import net.imglib2.type.Type;
 
-public interface PaintPixelsGenerator< T extends Type< T >, S extends Iterator< T > & Localizable >
+public interface PaintPixelsGenerator< T, S extends Iterator< T > & Localizable >
 {
 
 	public S getPaintPixels( RandomAccessible< T > accessible, RealLocalizable position, int timestep, int size );
