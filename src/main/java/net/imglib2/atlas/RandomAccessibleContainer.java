c09a1ef00c527f6b9ea656d792d403e8d6f7af2f
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2017-04-14 20:18:50 -0400

    Add RandomAccessibleContainer

---
+++ src/main/java/net/imglib2/atlas/RandomAccessibleContainer.java
@@ -0,0 +1,64 @@
+package net.imglib2.atlas;
+
+import java.util.HashSet;
+
+import net.imglib2.Interval;
+import net.imglib2.RandomAccess;
+import net.imglib2.RandomAccessible;
+
+public class RandomAccessibleContainer< T > implements RandomAccessible< T >
+{
+
+	public static interface SourceChangeListener< T >
+	{
+		public void notifyOnSourceChange( RandomAccessible< T > oldSource, RandomAccessible< T > newSource );
+	}
+
+	private RandomAccessible< T > source;
+
+	private final HashSet< SourceChangeListener< T > > listeners;
+
+	public RandomAccessibleContainer( final RandomAccessible< T > source )
+	{
+		super();
+		this.source = source;
+		this.listeners = new HashSet<>();
+	}
+
+	public void setSource( final RandomAccessible< T > source )
+	{
+		listeners.forEach( l -> l.notifyOnSourceChange( this.source, source ) );
+		this.source = source;
+	}
+
+	public void addListener( final SourceChangeListener< T > listener )
+	{
+		this.listeners.add( listener );
+	}
+
+	public boolean removeListener( final SourceChangeListener< T > listener )
+	{
+		return this.listeners.remove( listener );
+	}
+
+
+
+	@Override
+	public RandomAccess< T > randomAccess()
+	{
+		return source.randomAccess();
+	}
+
+	@Override
+	public RandomAccess< T > randomAccess( final Interval interval )
+	{
+		return source.randomAccess( interval );
+	}
+
+	@Override
+	public int numDimensions()
+	{
+		return source.numDimensions();
+	}
+
+}
