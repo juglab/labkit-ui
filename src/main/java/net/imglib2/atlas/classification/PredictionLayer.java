6c7bab0cf05900bf94b5501cf4ae8e6e23b838f1
Matthias Arzt <arzt@mpi-cbg.de>
2017-10-19 12:04:46 +0200

    Refacetor ProdictionLayer and ClassifyingCellLoader

--- src/main/java/net/imglib2/atlas/classification/PredictionLayer.java
+++ src/main/java/net/imglib2/atlas/classification/PredictionLayer.java
@@ -1,16 +1,18 @@
 package net.imglib2.atlas.classification;
 
 import net.imglib2.RandomAccessible;
+import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.atlas.FeatureStack;
 import net.imglib2.atlas.MainFrame;
 import net.imglib2.atlas.RandomAccessibleContainer;
 import net.imglib2.atlas.color.ColorMapProvider;
-import net.imglib2.atlas.control.brush.LabelBrushController;
+import net.imglib2.cache.img.CellLoader;
 import net.imglib2.cache.img.DiskCachedCellImgFactory;
 import net.imglib2.cache.img.DiskCachedCellImgOptions;
 import net.imglib2.converter.Converter;
 import net.imglib2.converter.Converters;
 import net.imglib2.img.Img;
+import net.imglib2.img.cell.CellGrid;
 import net.imglib2.type.numeric.ARGBType;
 import net.imglib2.type.numeric.integer.ShortType;
 import net.imglib2.type.volatiles.VolatileARGBType;
@@ -18,12 +20,12 @@ import net.imglib2.type.volatiles.VolatileShortType;
 import net.imglib2.util.ConstantUtils;
 import net.imglib2.view.Views;
 
+import java.awt.*;
+
 public class PredictionLayer implements Classifier.Listener
 {
 
 	private final MainFrame.Extensible extensible;
-
-	private final ClassifyingCellLoader loader;
 
 	private final ColorMapProvider colorProvider;
 
@@ -37,7 +39,6 @@ public class PredictionLayer implements Classifier.Listener
 		final RandomAccessible< VolatileARGBType > emptyPrediction = ConstantUtils.constantRandomAccessible( new VolatileARGBType( 0 ), featureStack.grid().numDimensions());
 		this.extensible = extensible;
 		this.featureStack = featureStack;
-		this.loader = new ClassifyingCellLoader(featureStack.compatibleOriginal(), classifier);
 		this.colorProvider = colorProvider;
 		this.predictionContainer = new RandomAccessibleContainer<>( emptyPrediction );
 		extensible.addLayer(Views.interval(predictionContainer, featureStack.interval()), "prediction");
@@ -50,43 +51,54 @@ public class PredictionLayer implements Classifier.Listener
 		if ( classifier.isTrained() )
 			synchronized ( extensible.viewerSync() )
 			{
-				prediction = getPrediction();
-
-				int[] colors = classifier.classNames().stream().map(colorProvider.colorMap()::getColor).mapToInt(ARGBType::get).toArray();
-
-				final Converter< VolatileShortType, VolatileARGBType > conv = ( input, output ) -> {
-					final boolean isValid = input.isValid();
-					output.setValid( isValid );
-					if ( isValid )
-						output.set(colors[input.get().get()]);
-				};
-
-				final RandomAccessible< VolatileShortType > extended =
-						Views.extendValue( extensible.wrapAsVolatile(prediction), new VolatileShortType( ( short ) LabelBrushController.BACKGROUND ) );
-				final RandomAccessible< VolatileARGBType > converted = Converters.convert( extended, conv, new VolatileARGBType() );
-
-				predictionContainer.setSource( converted );
-
+				updatePrediction(classifier);
+				updateContainer(classifier);
 				extensible.repaint();
 			}
 		else
-			predictionContainer.setSource(ConstantUtils.constantRandomAccessible(new VolatileARGBType(0), predictionContainer.numDimensions()));
+			predictionContainer.setSource(ConstantUtils.constantRandomAccessible(new VolatileARGBType(Color.red.getRGB()), predictionContainer.numDimensions()));
+	}
+
+	private void updateContainer(Classifier classifier) {
+		final RandomAccessibleInterval<VolatileARGBType> converted = coloredVolatileView(classifier);
+		predictionContainer.setSource(Views.extendValue( converted, new VolatileARGBType(0)  ));
+	}
+
+	private RandomAccessibleInterval<VolatileARGBType> coloredVolatileView(Classifier classifier) {
+		ARGBType[] colors = classifier.classNames().stream()
+				.map(colorProvider.colorMap()::getColor)
+				.toArray(ARGBType[]::new);
+
+		return mapColors(colors, extensible.wrapAsVolatile(prediction));
+	}
+
+	private RandomAccessibleInterval<VolatileARGBType> mapColors(ARGBType[] colors, RandomAccessibleInterval<VolatileShortType> source) {
+		final Converter< VolatileShortType, VolatileARGBType > conv = ( input, output ) -> {
+			final boolean isValid = input.isValid();
+			output.setValid( isValid );
+			if ( isValid )
+				output.set(colors[input.get().get()].get());
+		};
+
+		return Converters.convert(source, conv, new VolatileARGBType() );
 	}
 
 	public Img<ShortType> prediction() {
 		return prediction;
 	}
 
-	private Img<ShortType> getPrediction() {
-		final int[] cellDimensions = new int[ featureStack.grid().numDimensions() ];
-		featureStack.grid().cellDimensions( cellDimensions );
-		final DiskCachedCellImgOptions factoryOptions = DiskCachedCellImgOptions.options()
+	private void updatePrediction(Classifier classifier) {
+		final DiskCachedCellImgFactory< ShortType > factory = new DiskCachedCellImgFactory<>(setupDiskCachedCellImgOptions(featureStack.grid()));
+		CellLoader<ShortType> loader = target -> classifier.segment(featureStack.compatibleOriginal(), target);
+		prediction = factory.create( featureStack.grid().getImgDimensions(), new ShortType(), loader );
+	}
+
+	private DiskCachedCellImgOptions setupDiskCachedCellImgOptions(CellGrid grid) {
+		final int[] cellDimensions = new int[ grid.numDimensions() ];
+		grid.cellDimensions( cellDimensions );
+		return DiskCachedCellImgOptions.options()
 				//.cacheType( CacheType.BOUNDED )
 				//.maxCacheSize( 1000 )
 				.cellDimensions( cellDimensions );
-		final DiskCachedCellImgFactory< ShortType > factory = new DiskCachedCellImgFactory<>( factoryOptions );
-
-
-		return factory.create( featureStack.grid().getImgDimensions(), new ShortType(), loader );
 	}
 }
