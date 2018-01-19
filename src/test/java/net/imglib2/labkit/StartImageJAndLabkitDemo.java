d63f22590595868b817125933fc070aa010b8485
Matthias Arzt <arzt@mpi-cbg.de>
2018-01-19 14:29:34 +0100

    Demo: start ImageJ and Labkit opening "Leaf"

---
+++ src/test/java/net/imglib2/labkit/StartImageJAndLabkitDemo.java
@@ -0,0 +1,17 @@
+package net.imglib2.labkit;
+
+import net.imagej.ImageJ;
+import net.imglib2.labkit.plugin.LabkitPlugin;
+
+import java.io.IOException;
+
+public class StartImageJAndLabkitDemo {
+
+	public static void main(String... args) throws IOException {
+		ImageJ imageJ = new ImageJ();
+		imageJ.ui().showUI();
+		Object data = imageJ.io().open("https://imagej.nih.gov/ij/images/leaf.jpg");
+		imageJ.ui().show(data);
+		imageJ.command().run(LabkitPlugin.class, true);
+	}
+}
