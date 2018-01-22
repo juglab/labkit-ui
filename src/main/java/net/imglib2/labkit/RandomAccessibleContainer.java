2031de790f5a0adea8ab0c47172de45212cc504d
Matthias Arzt <arzt@mpi-cbg.de>
2018-01-22 15:13:15 +0100

    Remove unused code

--- src/main/java/net/imglib2/labkit/RandomAccessibleContainer.java
+++ src/main/java/net/imglib2/labkit/RandomAccessibleContainer.java
@@ -9,43 +9,18 @@ import net.imglib2.RandomAccessible;
 public class RandomAccessibleContainer< T > implements RandomAccessible< T >
 {
 
-	public RandomAccessible<T> getSource() {
-		return source;
-	}
-
-	public static interface SourceChangeListener< T >
-	{
-		public void notifyOnSourceChange( RandomAccessible< T > oldSource, RandomAccessible< T > newSource );
-	}
-
 	private RandomAccessible< T > source;
-
-	private final HashSet< SourceChangeListener< T > > listeners;
 
 	public RandomAccessibleContainer( final RandomAccessible< T > source )
 	{
 		super();
 		this.source = source;
-		this.listeners = new HashSet<>();
 	}
 
 	public void setSource( final RandomAccessible< T > source )
 	{
-		listeners.forEach( l -> l.notifyOnSourceChange( this.source, source ) );
 		this.source = source;
 	}
-
-	public void addListener( final SourceChangeListener< T > listener )
-	{
-		this.listeners.add( listener );
-	}
-
-	public boolean removeListener( final SourceChangeListener< T > listener )
-	{
-		return this.listeners.remove( listener );
-	}
-
-
 
 	@Override
 	public RandomAccess< T > randomAccess()
