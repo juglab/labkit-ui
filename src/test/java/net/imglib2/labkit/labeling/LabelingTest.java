215e16e5f1164e997201abcd4dd2bb25166c0370
Matthias Arzt <arzt@mpi-cbg.de>
2018-05-21 11:46:35 +0200

    Add test for label add and rename

--- src/test/java/net/imglib2/labkit/labeling/LabelingTest.java
+++ src/test/java/net/imglib2/labkit/labeling/LabelingTest.java
@@ -1,23 +1,50 @@
 
 package net.imglib2.labkit.labeling;
 
+import net.imglib2.FinalInterval;
 import net.imglib2.RandomAccess;
 import net.imglib2.util.Intervals;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class LabelingTest {
+
+	private final FinalInterval interval = Intervals.createMinSize(0, 0, 2, 2);
+
+	@Test
+	public void testAddLabel() {
+		Labeling labeling = new Labeling(Collections.emptyList(), interval);
+		String label = "foobar";
+		labeling.addLabel(label);
+		assertTrue(labeling.getLabels().contains(label));
+	}
+
+	@Test
+	public void testRemoveLabel() {
+		Labeling labeling = new Labeling(Arrays.asList("f", "b"), interval);
+		long[] position = { 0, 0 };
+		addPixelLabel(labeling, "f", position);
+		addPixelLabel(labeling, "b", position);
+		labeling.removeLabel("b");
+		// test
+		assertFalse(labeling.getLabels().contains("b"));
+		assertEquals(Collections.singletonList("f"), new ArrayList<>(getPixelLabels(
+			labeling, position)));
+	}
 
 	@Test
 	public void testRenameLabel() {
 		// setup
 		Labeling labeling = new Labeling(Arrays.asList("foreground", "background"),
-			Intervals.createMinSize(0, 0, 2, 2));
+			interval);
 		addPixelLabel(labeling, "foreground", 0, 0);
 		addPixelLabel(labeling, "background", 1, 1);
 		// process
@@ -46,7 +73,7 @@ public class LabelingTest {
 	public void testRenameLabel2() {
 		// setup
 		Labeling labeling = new Labeling(Arrays.asList("foreground", "background"),
-			Intervals.createMinSize(0, 0, 2, 2));
+			interval);
 		// process
 		labeling.renameLabel("foreground", "fg");
 		// test
