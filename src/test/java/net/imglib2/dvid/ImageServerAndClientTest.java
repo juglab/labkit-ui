72a232d887f4b7a21bd21d6fdd9fb7606620279e
Matthias Arzt <arzt@mpi-cbg.de>
2018-11-19 12:02:43 +0100

    Refactor ImageServerAndClientTest

--- src/test/java/net/imglib2/dvid/ImageServerAndClientTest.java
+++ src/test/java/net/imglib2/dvid/ImageServerAndClientTest.java
@@ -15,43 +15,52 @@ import net.imglib2.type.numeric.integer.UnsignedShortType;
 import net.imglib2.type.numeric.real.DoubleType;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.util.Intervals;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 public class ImageServerAndClientTest {
 
-	@Test
-	public void test() {
-		try(ImageServer server = new ImageServer()) {
-			test(new UnsignedByteType(42));
-			test(new ByteType((byte) 42));
-			test(new UnsignedShortType(42));
-			test(new ShortType((short) 42));
-			test(new UnsignedIntType(42));
-			test(new IntType(42));
-			test(new UnsignedLongType(42));
-			test(new LongType(42));
-			test(new FloatType(42));
-			test(new DoubleType(42));
-			System.out.println("sucess");
-		}
+	private ImageServer server;
+
+	@Before
+	public void startServer() {
+		server = new ImageServer();
 	}
 
-	public static <T extends NativeType<T>> void test(T type) {
+	@After
+	public void stopServer() {
+		server.close();
+	}
+
+	@Test
+	public void testTypes() {
+		testType(new UnsignedByteType(42));
+		testType(new ByteType((byte) 42));
+		testType(new UnsignedShortType(42));
+		testType(new ShortType((short) 42));
+		testType(new UnsignedIntType(42));
+		testType(new IntType(42));
+		testType(new UnsignedLongType(42));
+		testType(new LongType(42));
+		testType(new FloatType(42));
+		testType(new DoubleType(42));
+	}
+
+	private <T extends NativeType<T>> void testType(T type) {
 		Img<T> image = new ArrayImgFactory<>(type).create(1, 1, 1);
 		image.forEach(x -> x.set(type));
 		testImage(image);
 	}
 
-	public static <T extends NativeType<T>> void testImage(Img<T> image) {
+	private <T extends NativeType<T>> void testImage(Img<T> image) {
 		ImageId id = ImageRepository.getInstance().addImage("image", image);
+		@SuppressWarnings("unchecked")
 		Img<T> result = (Img<T>) ImageClient.asCachedImg(id.getUrl());
-		if( !Intervals.equals(image, result) )
-			throw new AssertionError("intervals different");
-		LoopBuilder.setImages(image, result).forEachPixel(
-				(a,b) -> {
-					if( !a.equals(b) )
-						throw new AssertionError("wrong pixel value");
-				}
-		);
+		assertTrue(Intervals.equals(image, result));
+		LoopBuilder.setImages(image, result).forEachPixel( Assert::assertEquals );
 	}
 }
