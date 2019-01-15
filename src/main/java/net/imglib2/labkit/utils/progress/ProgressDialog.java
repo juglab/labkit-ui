c54b8fb09f92f74c3ab9bad5248b03a73e6c341d
Matthias Arzt <arzt@mpi-cbg.de>
2019-01-15 17:51:32 +0100

    SwingProgressWriter: add ProgressDialog
    Add a ProgressDialog that is used instead of ProgressMonitor.

---
+++ src/main/java/net/imglib2/labkit/utils/progress/ProgressDialog.java
@@ -0,0 +1,105 @@
+
+package net.imglib2.labkit.utils.progress;
+
+import net.miginfocom.swing.MigLayout;
+
+import javax.swing.*;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.beans.PropertyChangeEvent;
+
+public class ProgressDialog {
+
+	private final JDialog dialog;
+	private final JLabel note = new JLabel("");
+	private final JProgressBar progressBar = new JProgressBar(0, 1000);
+	private final DetailsPane details = new DetailsPane();
+	private boolean canceled = false;
+
+	public ProgressDialog(Frame parent, String text) {
+		JOptionPane pane = new JOptionPane(new Object[] { text, note, progressBar,
+			details }, JOptionPane.INFORMATION_MESSAGE, JOptionPane.OK_CANCEL_OPTION,
+			null, new Object[] { "Hide", "Cancel" });
+		pane.addPropertyChangeListener(this::buttonClicked);
+		dialog = pane.createDialog(parent, text);
+		dialog.setResizable(true);
+		dialog.setModal(false);
+		dialog.add(pane);
+		dialog.pack();
+	}
+
+	private void buttonClicked(PropertyChangeEvent event) {
+		if (event.getPropertyName().equals(JOptionPane.VALUE_PROPERTY)) if (event
+			.getNewValue().equals("Cancel")) canceled = true;
+	}
+
+	public void setNote(String note) {
+		this.note.setText(note);
+	}
+
+	public void setProgress(double progress) {
+		progressBar.getModel().setValue((int) (progress * 1000));
+		if (progress < 1.0) {
+			if (!canceled) dialog.setVisible(true);
+		}
+		else dialog.dispose();
+	}
+
+	public void addDetails(String line) {
+		details.add(line);
+	}
+
+	public static void main(String... args) throws InterruptedException {
+		ProgressDialog dialog = new ProgressDialog(null,
+			"Demonstrate ProgressDialog");
+		for (int i = 0; i < 5; i++) {
+			dialog.setProgress((double) i / 5.0);
+			dialog.setNote("Step " + i + " of " + 5);
+			Thread.sleep(1000);
+			dialog.addDetails("Step " + i + " completed\n");
+		}
+		dialog.setProgress(1.0);
+		for (int i = 0; i < 5; i++) {
+			dialog.setProgress((double) i / 5.0);
+			dialog.setNote("Step " + i + " of " + 5);
+			Thread.sleep(1000);
+			dialog.addDetails("Step " + i + " completed\n");
+		}
+		dialog.setProgress(1.0);
+	}
+
+	public boolean isCanceled() {
+		return canceled;
+	}
+
+	private static class DetailsPane extends JPanel {
+
+		private final JTextArea text;
+
+		DetailsPane() {
+			text = new JTextArea();
+			text.setBackground(Color.BLACK);
+			text.setForeground(Color.LIGHT_GRAY);
+			text.setFont(new Font(Font.MONOSPACED, Font.BOLD, 12));
+			final JScrollPane scrollPane = new JScrollPane(text);
+			scrollPane.setVisible(false);
+			setLayout(new MigLayout("insets 0", "[grow]", "[][grow]"));
+			final JCheckBox show_details = new JCheckBox("show Details");
+			show_details.addItemListener(event -> {
+				scrollPane.setVisible(event.getStateChange() == ItemEvent.SELECTED);
+			});
+			add(show_details, "wrap");
+			add(scrollPane, "grow");
+		}
+
+		public void add(String line) {
+			final Document document = text.getDocument();
+			try {
+				document.insertString(document.getLength(), line + "\n", null);
+			}
+			catch (BadLocationException e) {}
+		}
+	}
+}
--- src/main/java/net/imglib2/labkit/utils/progress/SwingProgressWriter.java
+++ src/main/java/net/imglib2/labkit/utils/progress/SwingProgressWriter.java
@@ -9,16 +9,14 @@ import java.util.concurrent.CancellationException;
 
 public class SwingProgressWriter implements ProgressWriter {
 
-	private final ProgressMonitor dialog;
+	private final ProgressDialog dialog;
 	private final PrintStream out = PrintStreamToLines.printStreamToLines(
 		this::showNote);
 	private final PrintStream err = PrintStreamToLines.printStreamToLines(
 		this::showNote);
 
 	public SwingProgressWriter(JFrame dialogParent, String title) {
-		dialog = new ProgressMonitor(dialogParent, title, null, 0, 1000);
-		dialog.setMillisToPopup(0);
-		dialog.setMillisToDecideToPopup(0);
+		dialog = new ProgressDialog(dialogParent, title);
 	}
 
 	@Override
@@ -33,15 +31,12 @@ public class SwingProgressWriter implements ProgressWriter {
 
 	private void showNote(String line) {
 		if (!line.isEmpty()) dialog.setNote(line);
+		dialog.addDetails(line);
 	}
 
 	@Override
 	public void setProgress(double completionRatio) {
-		dialog.setProgress((int) (completionRatio * 1000));
+		dialog.setProgress(completionRatio);
 		if (dialog.isCanceled()) throw new CancellationException();
-	}
-
-	public void showProgress(int stop, int total) {
-		setProgress((double) stop / (double) total);
 	}
 }
