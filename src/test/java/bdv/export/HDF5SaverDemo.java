438b3e972b2d3b547769921a9a5b51c45912f9ae
Matthias Arzt <arzt@mpi-cbg.de>
2019-01-15 17:53:40 +0100

    Fix HDF5SaveDemo
    Use correct 3d image. Don't use Views.concatenate as it has some serious
    performance problems.

--- src/test/java/bdv/export/HDF5SaverDemo.java
+++ src/test/java/bdv/export/HDF5SaverDemo.java
@@ -1,19 +1,11 @@
 
 package bdv.export;
 
-import bdv.ViewerSetupImgLoader;
-import bdv.spimdata.SequenceDescriptionMinimal;
-import bdv.spimdata.SpimDataMinimal;
-import bdv.spimdata.XmlIoSpimDataMinimal;
-import bdv.util.BdvFunctions;
 import ij.ImagePlus;
 import mpicbg.spim.data.SpimDataException;
-import mpicbg.spim.data.generic.sequence.BasicViewSetup;
-import mpicbg.spim.data.sequence.TimePoint;
-import net.imagej.ImgPlus;
+import net.imglib2.FinalInterval;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.converter.Converters;
-import net.imglib2.img.Img;
 import net.imglib2.img.VirtualStackAdapter;
 import net.imglib2.labkit.utils.progress.SwingProgressWriter;
 import net.imglib2.type.numeric.RealType;
@@ -22,7 +14,6 @@ import net.imglib2.view.Views;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
 public class HDF5SaverDemo {
 
@@ -35,9 +26,9 @@ public class HDF5SaverDemo {
 		// cancel button is clicked.
 		RandomAccessibleInterval<T> image =
 			(RandomAccessibleInterval<T>) VirtualStackAdapter.wrap(new ImagePlus(
-				"https://imagej.nih.gov/ij/images/t1-head.gif"));
-		image = Views.concatenate(0, image, image, image, image);
-		image = Views.concatenate(1, image, image, image, image);
+				"https://imagej.nih.gov/ij/images/t1-head.zip"));
+		image = Views.interval(Views.extendPeriodic(image), new FinalInterval(1000,
+			1000, 1000));
 		RandomAccessibleInterval<UnsignedShortType> result = treshold(image);
 		HDF5Saver saver = new HDF5Saver();
 		saver.setProgressWriter(new SwingProgressWriter(null, "Save Huge Image"));
