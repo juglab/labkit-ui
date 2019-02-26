372ab0d0e97d9537d9032dc1dc2db4bfe7481028
Matthias Arzt <arzt@mpi-cbg.de>
2019-02-26 16:26:18 +0100

    TrainableWeka3DLegacyDemo: don't use images stored on disk

--- src/test/java/net/imglib2/labkit_trainable_weka/TrainableWeka3DLegacyDemo.java
+++ src/test/java/net/imglib2/labkit_trainable_weka/TrainableWeka3DLegacyDemo.java
@@ -1,37 +1,47 @@
 
 package net.imglib2.labkit_trainable_weka;
 
-import bdv.util.BdvFunctions;
-import bdv.util.BdvOptions;
 import ij.ImagePlus;
-import net.imglib2.img.ImagePlusAdapter;
-import net.imglib2.img.Img;
+import ij.ImageStack;
+import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.img.display.imagej.ImageJFunctions;
-import net.imglib2.type.numeric.RealType;
 import trainableSegmentation.WekaSegmentation;
+
+import java.util.Arrays;
 
 public class TrainableWeka3DLegacyDemo {
 
 	public static void main(String... args) {
-		ImagePlus image = new ImagePlus(
-			"/home/arzt/Documents/Datasets/Tiny Head/small-head.tif");
-		ImagePlus labels = (new ImagePlus(
-			"/home/arzt/Documents/Datasets/Tiny Head/small-head.tif.labels.tif"));
+		// setup images
+		ImagePlus image = asZStack(new byte[] { 1, 0, 0, 1, 1, 0, 0, 1 });
+		ImagePlus labels = asZStack(new byte[] { 1, 0, -1, -1, -1, -1, -1, -1 });
+		// train classifier
 		WekaSegmentation wekaSegmentation = new WekaSegmentation(true);
-		wekaSegmentation.setClassLabels(new String[] { "background",
-			"foreground" });
-		subtractOne(labels);
 		wekaSegmentation.addLabeledData(image, labels);
 		wekaSegmentation.trainClassifier();
+		// apply classifier
 		ImagePlus result = wekaSegmentation.applyClassifier(image);
-		final Img<? extends RealType<?>> wrap = ImageJFunctions.wrapReal(result);
-		BdvFunctions.show(wrap, "title", BdvOptions.options().is2D())
-			.setDisplayRange(0, 2);
+		// show result
+		// result.show();
+		System.out.println("Segmentation: " + Arrays.toString(getPixels(result)));
 	}
 
-	private static ImagePlus subtractOne(ImagePlus labels) {
-		ImagePlusAdapter.wrapFloat(labels).forEach(x -> x.setReal(x
-			.getRealDouble() - 1));
-		return labels;
+	private static ImagePlus asZStack(byte[] imagePixels) {
+		// NB: This is hard coded to a image size of 2x2x2 pixels.
+		ImagePlus image = ImageJFunctions.wrap(ArrayImgs.unsignedBytes(imagePixels,
+			2, 2, 2), "title");
+		image.setStack(image.getStack(), 1, 2, 1);
+		return image;
+	}
+
+	private static byte[] getPixels(ImagePlus imagePlus) {
+		final ImageStack stack = imagePlus.getStack();
+		int planeSize = stack.getWidth() * stack.getHeight();
+		byte[] pixels = new byte[planeSize * stack.getSize()];
+		for (int i = 0; i < stack.getSize(); i++) {
+			System.arraycopy(stack.getPixels(i + 1), 0, pixels, i * planeSize,
+				planeSize);
+		}
+		return pixels;
 	}
 }
