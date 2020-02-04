4649d421c9706914a8530596741e356643e7aba2
Matthias Arzt <arzt@mpi-cbg.de>
2020-02-04 18:01:08 +0100

    Add demo: how an segmentation algorithm can use the Labkit UI.

---
+++ src/test/java/demo/mats_2/MatsDemo2.java
@@ -0,0 +1,26 @@
+
+package demo.mats_2;
+
+import ij.ImagePlus;
+import net.imglib2.img.Img;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.labkit.LabkitFrame;
+import net.imglib2.labkit.inputimage.DefaultInputImage;
+import net.imglib2.labkit.models.DefaultSegmentationModel;
+import net.imglib2.type.numeric.NumericType;
+import org.scijava.Context;
+
+/**
+ * This shows
+ */
+public class MatsDemo2 {
+
+	public static void main(String... args) {
+		Img<? extends NumericType<?>> image = ImageJFunctions.wrap(new ImagePlus(
+			"/home/arzt/Desktop/original1.tif"));
+		Context context = new Context();
+		DefaultSegmentationModel segmentationModel = new DefaultSegmentationModel(
+			new DefaultInputImage(image), context, YourSegmenter::new);
+		LabkitFrame.show(segmentationModel, "Demonstrate other Segmenter");
+	}
+}
---
+++ src/test/java/demo/mats_2/YourSegmenter.java
@@ -0,0 +1,133 @@
+
+package demo.mats_2;
+
+import net.imglib2.Cursor;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.labkit.inputimage.InputImage;
+import net.imglib2.labkit.labeling.Labeling;
+import net.imglib2.labkit.segmentation.Segmenter;
+import net.imglib2.type.numeric.IntegerType;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.util.Pair;
+import net.imglib2.view.Views;
+import org.scijava.Context;
+
+import javax.swing.*;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * This class is supposed to show YOU how to implement a segmentation algorithm
+ * such that it can be used within the Labkit UI. See {@link MatsDemo2} to run
+ * the demo.
+ */
+class YourSegmenter implements Segmenter {
+
+	private boolean isTrained = false;
+
+	public YourSegmenter(final Context context, final InputImage inputImage) {
+
+	}
+
+	@Override
+	public void editSettings(JFrame dialogParent) {
+
+	}
+
+	/**
+	 * This method is called, when the user click on train (the little play button
+	 * like triangle)
+	 *
+	 * @param data A list of pairs. Each pair, is a combination of: 1. An image to
+	 *          be used for training together. 2. The "drawing" made by the user.
+	 */
+	@Override
+	public void train(
+		List<Pair<? extends RandomAccessibleInterval<?>, ? extends Labeling>> data)
+	{
+		System.out.println("MySegmenter.train(...) was called.");
+
+		//// The following lines will show the training data, (if you uncomment
+		//// them)
+
+		// for(Pair<? extends RandomAccessibleInterval<?>, ? extends Labeling> pair
+		// : data) {
+		// ImagePlus originalImage =
+		// ImageJFunctions.wrap(Casts.unchecked(pair.getA()), "original image");
+		// originalImage.show();
+		// ImagePlus drawings =
+		// ImageJFunctions.wrap(Casts.unchecked(pair.getB().getIndexImg()),
+		// "drawings");
+		// drawings.show();
+		// }
+
+		isTrained = true;
+	}
+
+	/**
+	 * This method is supposed to calculate the automatic segmentation.
+	 *
+	 * @param image The image to be segmented.
+	 * @param outputSegmentation The segmentation result needs to be written to
+	 *          this image.
+	 */
+	@Override
+	public void segment(RandomAccessibleInterval<?> image,
+		RandomAccessibleInterval<? extends IntegerType<?>> outputSegmentation)
+	{
+		System.out.println("MySegmenter.segment(...) was called.");
+
+		//// The following lines will show the image if you uncomment them
+
+		// ImagePlus originalImage = ImageJFunctions.wrap(Casts.unchecked(image),
+		// "Method segment image parameter");
+		// originalImage.show();
+
+		drawCircle(outputSegmentation);
+	}
+
+	private void drawCircle(
+		RandomAccessibleInterval<? extends IntegerType<?>> outputSegmentation)
+	{
+		Cursor<? extends IntegerType<?>> cursor = Views.iterable(outputSegmentation)
+			.cursor();
+		while (cursor.hasNext()) {
+			cursor.fwd();
+			long x = cursor.getLongPosition(0);
+			long y = cursor.getLongPosition(1);
+			long radius = square(x - 100) + square(y - 100);
+			cursor.get().setInteger(radius < square(50) ? 1 : 0);
+		}
+	}
+
+	private static long square(long value) {
+		return value * value;
+	}
+
+	@Override
+	public void predict(RandomAccessibleInterval<?> image,
+		RandomAccessibleInterval<? extends RealType<?>> outputProbabilityMap)
+	{
+		// not needed
+	}
+
+	@Override
+	public boolean isTrained() {
+		return isTrained;
+	}
+
+	@Override
+	public void saveModel(String path) {
+
+	}
+
+	@Override
+	public void openModel(String path) {
+
+	}
+
+	@Override
+	public List<String> classNames() {
+		return Arrays.asList("background", "foreground");
+	}
+}
