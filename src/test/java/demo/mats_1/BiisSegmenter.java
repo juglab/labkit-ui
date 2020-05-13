8382317df58d35c59c6c84cdfaff7066afdf031a
Matthias Arzt <arzt@mpi-cbg.de>
2020-05-13 15:38:42 +0200

    Refactor MatsDemo1

--- src/test/java/demo/mats_1/BiisSegmenter.java
+++ src/test/java/demo/mats_1/BiisSegmenter.java
@@ -7,22 +7,28 @@ import ij.plugin.ChannelSplitter;
 
 public class BiisSegmenter {
 
-	public static ImagePlus run(ImagePlus input_img_org, ImagePlus manual_segmented_image_original) {
-		// render the input image to 8-bit once.
-		ImagePlus input_image = input_img_org.duplicate();
-		ImagePlus manual_segmented_image = manual_segmented_image_original.duplicate();
+	private float[] parameters;
 
-		IJ.run(input_image, "8-bit", "");
-
-		ImagePlus[] channels = ChannelSplitter.split(input_image);
-		input_image = channels[0];
-		IJ.run(input_image, "Grays", "");
-
+	public void train(ImagePlus image, ImagePlus manualSegmentation) {
+		ImagePlus input_image = asUnsignedByteType(image);
+		ImagePlus manual_segmented_image = manualSegmentation.duplicate();
 		Pipeline_fMN_fast pipe_fast = new Pipeline_fMN_fast(input_image);
 		BiisOptimizationProblem likelihood = new BiisOptimizationProblem(pipe_fast,
 			manual_segmented_image);
-		float[] result = new BayesOptimizer(likelihood).run();
+		parameters = new BayesOptimizer(likelihood).run();
+	}
 
-		return pipe_fast.exec(result); // pipeline generated image
+	public ImagePlus apply(ImagePlus image) {
+		Pipeline_fMN_fast pipe_fast = new Pipeline_fMN_fast(asUnsignedByteType(image));
+		return pipe_fast.exec(parameters); // pipeline generated image
+	}
+
+	private static ImagePlus asUnsignedByteType(ImagePlus input_img_org) {
+		ImagePlus input_image = input_img_org.duplicate();
+		IJ.run(input_image, "8-bit", "");
+		ImagePlus[] channels = ChannelSplitter.split(input_image);
+		input_image = channels[0];
+		IJ.run(input_image, "Grays", "");
+		return input_image;
 	}
 }
--- src/test/java/demo/mats_1/MatsDemo1.java
+++ src/test/java/demo/mats_1/MatsDemo1.java
@@ -26,13 +26,17 @@ public class MatsDemo1 {
 	public static void main(String... args) throws IOException {
 
 		// open the image
-		final ImagePlus image = new ImagePlus(
-			// "https://imagej.nih.gov/ij/images/blobs.gif");
-			// "https://cgcweb.med.tu-dresden.de/cloud/index.php/s/G2ML3C6yYtnZW82/download?path=%2F&files=original1.tif");
-			// "https://cgcweb.med.tu-dresden.de/cloud/index.php/s/G2ML3C6yYtnZW82/download?path=%2F&files=original2.tif");
-			"/home/arzt/Desktop/original1.tif");
+		// String pathOrURL = "https://imagej.nih.gov/ij/images/blobs.gif");
+		// String pathOrURL =
+		// "https://cgcweb.med.tu-dresden.de/cloud/index.php/s/G2ML3C6yYtnZW82/download?path=%2F&files=original1.tif");
+		// String pathOrURL =
+		// "https://cgcweb.med.tu-dresden.de/cloud/index.php/s/G2ML3C6yYtnZW82/download?path=%2F&files=original2.tif");
+		// String pathOrURL =
 		// "/home/mats/Dokumente/postdoc/conferences/fiji_hackathon_dd2019/project_BayesImaging/sampledata/1h+S7_und_S8_tiff+S7+S7_NaCl+/original1.tif");
+		// String pathOrURL =
 		// "https://cgcweb.med.tu-dresden.de/cloud/index.php/s/G2ML3C6yYtnZW82/download?path=%2Fbsp3&files=nativ.tif");
+		String pathOrURL = "/home/arzt/Desktop/original1.tif";
+		final ImagePlus image = new ImagePlus(pathOrURL);
 		// create empty labeling with a background label and the same size as the
 		// image
 		Labeling labeling = Labeling.createEmpty(Arrays.asList("background"),
@@ -68,7 +72,8 @@ public class MatsDemo1 {
 		*  */
 
 		BiisSegmenter segmenter = new BiisSegmenter();
-		ImagePlus pgi = segmenter.run(image, manual_segmented_image);
+		segmenter.train(image, manual_segmented_image);
+		ImagePlus pgi = segmenter.apply(image);
 
 		BdvStackSource<?> handle = BdvFunctions.show(VirtualStackAdapter.wrap(
 			image), image.getTitle(), BdvOptions.options().is2D());
