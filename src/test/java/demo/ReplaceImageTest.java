3d45dce638386e004624eeeea8e1a24d122fa870
Matthias Arzt <arzt@mpi-cbg.de>
2020-06-30 17:35:04 +0200

    ReplaceImageTest: add a model that holds images and visibility

--- src/test/java/demo/ReplaceImageTest.java
+++ src/test/java/demo/ReplaceImageTest.java
@@ -10,22 +10,16 @@ import bdv.viewer.ViewerStateChange;
 import ij.ImagePlus;
 import net.imagej.ImgPlus;
 import net.imglib2.img.VirtualStackAdapter;
-import net.imglib2.img.array.ArrayImg;
-import net.imglib2.img.array.ArrayImgs;
-import net.imglib2.img.basictypeaccess.array.ByteArray;
 import net.imglib2.labkit.bdv.BdvShowable;
 import net.imglib2.labkit.inputimage.SpimDataInputImage;
 import net.imglib2.labkit.models.DefaultHolder;
 import net.imglib2.labkit.models.Holder;
-import net.imglib2.test.RandomImgs;
-import net.imglib2.type.numeric.ARGBType;
-import net.imglib2.type.numeric.integer.UnsignedByteType;
 import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.io.File;
-import java.util.Random;
 
 public class ReplaceImageTest {
 
@@ -33,13 +27,55 @@ public class ReplaceImageTest {
 
 	public static void main(String... args) {
 		JFrame frame = new JFrame("Test");
-		BigDataViewerComponent hp = new BigDataViewerComponent(frame);
-		new ImageController(hp, "A", Color.red);
-		new ImageController(hp, "B", Color.green);
-		new ImageController(hp, "C", Color.blue);
+		Model model = new Model();
+		BigDataViewerComponent hp = new BigDataViewerComponent(frame, model);
+		new ImageController(model.image(), model.imageVisibility(), hp.getSidePanel(), "image");
+		new ImageController(model.labeling(), model.labelingVisibility(), hp.getSidePanel(),
+			"labeling");
+		new ImageController(model.segmentation(), model.segmentationVisibility(), hp.getSidePanel(),
+			"segmentation");
 		frame.add(hp);
 		frame.pack();
 		frame.setVisible(true);
+	}
+
+	private static class Model {
+
+		private final Holder<BdvShowable> image = new DefaultHolder<>(null);
+
+		private final Holder<Boolean> imageVisibility = new DefaultHolder<>(true);
+
+		private final Holder<BdvShowable> labeling = new DefaultHolder<>(null);
+
+		private final Holder<Boolean> labelingVisibility = new DefaultHolder<>(true);
+
+		private final Holder<BdvShowable> segmentation = new DefaultHolder<>(null);
+
+		private final Holder<Boolean> segmentationVisibility = new DefaultHolder<>(true);
+
+		public Holder<BdvShowable> image() {
+			return image;
+		}
+
+		public Holder<Boolean> imageVisibility() {
+			return imageVisibility;
+		}
+
+		public Holder<BdvShowable> labeling() {
+			return labeling;
+		}
+
+		public Holder<Boolean> labelingVisibility() {
+			return labelingVisibility;
+		}
+
+		public Holder<BdvShowable> segmentation() {
+			return segmentation;
+		}
+
+		public Holder<Boolean> segmentationVisibility() {
+			return segmentationVisibility;
+		}
 	}
 
 	private static class BigDataViewerComponent extends JPanel {
@@ -48,10 +84,13 @@ public class ReplaceImageTest {
 
 		private final JPanel sidePanel;
 
-		private BigDataViewerComponent(JFrame frame) {
-			handle = new BdvHandlePanel(frame, Bdv.options());
+		private BigDataViewerComponent(JFrame frame, Model model) {
+			this.handle = new BdvHandlePanel(frame, Bdv.options());
 			handle.getViewerPanel().setMinimumSize(new Dimension(500, 500));
 			handle.getViewerPanel().setPreferredSize(new Dimension(500, 500));
+			new BigDataViewEntry(handle, model.image(), model.imageVisibility());
+			new BigDataViewEntry(handle, model.labeling(), model.labelingVisibility());
+			new BigDataViewEntry(handle, model.segmentation(), model.segmentationVisibility());
 			sidePanel = new JPanel();
 			sidePanel.setLayout(new MigLayout("", "[grow]"));
 			setLayout(new BorderLayout());
@@ -62,62 +101,91 @@ public class ReplaceImageTest {
 		public JPanel getSidePanel() {
 			return sidePanel;
 		}
+	}
 
-		public BdvHandle getHandle() {
-			return handle;
+	private static class BigDataViewEntry {
+
+		private final BdvHandle handle;
+
+		private final Holder<BdvShowable> image;
+
+		private final Holder<Boolean> visibility;
+
+		private BdvStackSource<?> source;
+
+		public BigDataViewEntry(BdvHandle handle, Holder<BdvShowable> image,
+			Holder<Boolean> visibility)
+		{
+			this.handle = handle;
+			this.image = image;
+			this.visibility = visibility;
+			this.source = null;
+			handle.getViewerPanel().state().changeListeners().add(this::viewerStateChanged);
+			visibility.notifier().add(this::visibilityChanged);
+			image.notifier().add(this::imageChanged);
+			imageChanged();
+		}
+
+		private void imageChanged() {
+			BdvStackSource<?> source = this.source;
+			this.source = null;
+			if (source != null) source.removeFromBdv();
+			BdvShowable showable = image.get();
+			if (showable != null)
+				this.source = (BdvStackSource<?>) showable.show("title", BdvOptions.options().addTo(
+					handle));
+			visibilityChanged();
+		}
+
+		private void visibilityChanged() {
+			BdvStackSource<?> source = this.source;
+			if (source != null) {
+				Boolean visible = visibility.get();
+				source.setActive(visible);
+			}
+		}
+
+		private void viewerStateChanged(ViewerStateChange change) {
+			BdvStackSource<?> source = this.source;
+			if (source != null && change == ViewerStateChange.VISIBILITY_CHANGED) {
+				boolean visible = handle.getViewerPanel().state().isSourceActive(source.getSources().get(
+					0));
+				visibility.set(visible);
+			}
 		}
 	}
 
 	private static class ImageController {
 
-		private final Random random = new Random();
-
 		private final JButton button;
 
 		private final JCheckBox checkBox;
 
-		private final BigDataViewerComponent bdvComponent;
+		private final Holder<BdvShowable> image;
 
-		private BdvStackSource<?> source;
+		private final Holder<Boolean> visibility;
 
-		private final Color color;
-
-		private ImageController(BigDataViewerComponent bdvComponent, String title, Color color) {
-			this.color = color;
-			this.bdvComponent = bdvComponent;
-			addSource(bdvComponent.getHandle());
+		private ImageController(Holder<BdvShowable> image, Holder<Boolean> visibility, JPanel sidePanel,
+			String title)
+		{
+			this.image = image;
+			this.visibility = visibility;
 			this.button = new JButton("change " + title);
 			button.addActionListener(ignore -> openImage());
-			bdvComponent.getSidePanel().add(button);
+			sidePanel.add(button);
 			this.checkBox = new JCheckBox("show " + title);
-			checkBox.addActionListener(event -> setSourceActive(checkBox.isSelected()));
-			bdvComponent.getHandle().getViewerPanel().state().changeListeners().add(change -> {
-				if (change == ViewerStateChange.VISIBILITY_CHANGED) {
-					updateCheckbox();
-				}
-			});
-			updateCheckbox();
-			bdvComponent.getSidePanel().add(checkBox, "wrap");
+			checkBox.addActionListener(this::checkBoxChanged);
+			visibility.notifier().add(this::visibilityChanged);
+			visibilityChanged();
+			sidePanel.add(checkBox, "wrap");
 		}
 
-		private void setSourceActive(boolean selected) {
-			source.setActive(selected);
+		private void checkBoxChanged(ActionEvent ignore) {
+			visibility.set(checkBox.isSelected());
 		}
 
-		private void updateCheckbox() {
-			boolean visible = this.bdvComponent.getHandle().getViewerPanel().state().isSourceActive(source
-				.getSources().get(0));
-			checkBox.setSelected(visible);
-		}
-
-		private void addSource(BdvHandle handle) {
-			if (source != null)
-				source.removeFromBdv();
-			ArrayImg<UnsignedByteType, ByteArray> image = ArrayImgs.unsignedBytes(100, 100, 100);
-			RandomImgs.seed(random.nextInt()).randomize(image);
-			source = (BdvStackSource) BdvShowable.wrap(image).show("title", BdvOptions.options().addTo(
-				handle));
-			source.setColor(new ARGBType(color.getRGB()));
+		private void visibilityChanged() {
+			checkBox.setSelected(visibility.get());
 		}
 
 		private void openImage() {
@@ -125,29 +193,19 @@ public class ReplaceImageTest {
 			if (returnValue == JFileChooser.APPROVE_OPTION) {
 				File file = fileChooser.getSelectedFile();
 				BdvShowable showable = openImage(file);
-				replaceSource(showable);
+				image.set(showable);
 			}
 		}
 
-		private void replaceSource(BdvShowable showable) {
-			if (source != null)
-				source.removeFromBdv();
-			source = (BdvStackSource) showable.show("title", BdvOptions.options().addTo(bdvComponent
-				.getHandle()));
-			updateCheckbox();
-		}
-
-		private BdvShowable openImage(File file) {
-			BdvShowable showable;
+		private static BdvShowable openImage(File file) {
 			if (file.getPath().endsWith(".xml")) {
-				showable = new SpimDataInputImage(file.getAbsolutePath(), 0).showable();
+				return new SpimDataInputImage(file.getAbsolutePath(), 0).showable();
 			}
 			else {
 				ImagePlus image = new ImagePlus(file.getAbsolutePath());
 				ImgPlus<?> imgPlus = VirtualStackAdapter.wrap(image);
-				showable = BdvShowable.wrap((ImgPlus) imgPlus);
+				return BdvShowable.wrap((ImgPlus) imgPlus);
 			}
-			return showable;
 		}
 	}
 }
