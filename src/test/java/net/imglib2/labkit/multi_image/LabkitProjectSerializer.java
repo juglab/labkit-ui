42a95faea61fb764f9632637f006a6e714660c7b
Matthias Arzt <arzt@mpi-cbg.de>
2020-07-13 13:35:46 +0200

    WIP: implement project serialization

--- src/test/java/net/imglib2/labkit/multi_image/LabkitProjectSerializer.java
+++ src/test/java/net/imglib2/labkit/multi_image/LabkitProjectSerializer.java
@@ -1,4 +1,28 @@
+
 package net.imglib2.labkit.multi_image;
 
-public class LabkitPojectSerializer {
+import net.imglib2.labkit.models.LabkitProjectModel;
+
+import java.io.File;
+import java.util.List;
+
+public class LabkitProjectSerializer {
+
+	private static class JacksonData {
+
+		public List<JacksonImage> images;
+
+		public JacksonData() {}
+	}
+
+	private static class JacksonImage {
+
+		public String nick_name;
+
+		public String image_file;
+
+		public String labeling_file;
+	}
+
+	public static void save(LabkitProjectModel project, File tmp) {}
 }
--- src/test/java/net/imglib2/labkit/multi_image/LabkitProjectSerializerTest.java
+++ src/test/java/net/imglib2/labkit/multi_image/LabkitProjectSerializerTest.java
@@ -1,4 +1,28 @@
+
 package net.imglib2.labkit.multi_image;
 
+import net.imglib2.labkit.models.LabeledImage;
+import net.imglib2.labkit.models.LabkitProjectModel;
+import net.imglib2.trainable_segmentation.utils.SingletonContext;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+
 public class LabkitProjectSerializerTest {
+
+	@Test
+	public void testJackson() throws IOException {
+		LabeledImage image = new LabeledImage("Carsten", "carsten.tif", "carsten.labeling");
+		LabkitProjectModel project = new LabkitProjectModel(SingletonContext.getInstance(), Collections
+			.singletonList(image));
+		File tmp = File.createTempFile("test", ".yaml");
+		tmp.deleteOnExit();
+		LabkitProjectSerializer.save(project, tmp);
+		String text = FileUtils.readFileToString(tmp, StandardCharsets.UTF_8);
+		System.out.println(text);
+	}
 }
