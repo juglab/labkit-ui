2d672d270d7afbf3c7ca800367da617884bd8af1
Matthias Arzt <arzt@mpi-cbg.de>
2020-07-29 15:33:41 +0200

    Make SegmenterListModel.segmenters() a Holder

--- src/main/java/net/imglib2/labkit/models/SegmenterListModel.java
+++ src/main/java/net/imglib2/labkit/models/SegmenterListModel.java
@@ -4,24 +4,21 @@ package net.imglib2.labkit.models;
 import net.imagej.ImgPlus;
 import net.imglib2.labkit.labeling.Labeling;
 import net.imglib2.labkit.segmentation.SegmentationPlugin;
-import net.imglib2.labkit.utils.Notifier;
 import net.imglib2.util.Pair;
 import net.imglib2.util.ValuePair;
 import org.scijava.Context;
 
 import java.util.AbstractList;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class SegmenterListModel {
 
 	private final Context context;
-	private final List<SegmentationItem> segmenters = new ArrayList<>();
+	private final Holder<List<SegmentationItem>> segmenters = new DefaultHolder<>(new ArrayList<>());
 	private final Holder<SegmentationItem> selectedSegmenter = new DefaultHolder<>(null);
 	private final Holder<Boolean> segmentationVisibility = new DefaultHolder<>(true);
 	private final Holder<List<Pair<ImgPlus<?>, Labeling>>> trainingData = new DefaultHolder<>(null);
-	private final Notifier listeners = new Notifier();
 	private final ImageLabelingModel imageLabelingModel;
 
 	public SegmenterListModel(Context context, ImageLabelingModel imageLabelingModel) {
@@ -30,8 +27,8 @@ public class SegmenterListModel {
 		this.trainingData.set(new SingletonTrainingData(imageLabelingModel));
 	}
 
-	public List<SegmentationItem> segmenters() {
-		return Collections.unmodifiableList(segmenters);
+	public Holder<List<SegmentationItem>> segmenters() {
+		return segmenters;
 	}
 
 	public Holder<SegmentationItem> selectedSegmenter() {
@@ -41,23 +38,19 @@ public class SegmenterListModel {
 	public SegmentationItem addSegmenter(SegmentationPlugin plugin) {
 		// TODO: make this a controller
 		SegmentationItem segmentationItem = new SegmentationItem(imageLabelingModel, plugin);
-		segmenters.add(segmentationItem);
-		listeners.notifyListeners();
+		segmenters.get().add(segmentationItem);
+		segmenters.notifier().notifyListeners();
 		return segmentationItem;
 	}
 
 	public void remove(SegmentationItem item) {
-		segmenters.remove(item);
-		if (!segmenters.contains(selectedSegmenter.get())) selectedSegmenter.set(null);
-		listeners.notifyListeners();
+		segmenters.get().remove(item);
+		if (!segmenters.get().contains(selectedSegmenter.get())) selectedSegmenter.set(null);
+		segmenters.notifier().notifyListeners();
 	}
 
 	public Holder<Boolean> segmentationVisibility() {
 		return segmentationVisibility;
-	}
-
-	public Notifier listChangeListeners() {
-		return listeners;
 	}
 
 	public Context context() {
