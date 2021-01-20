f1da242e87f2d0cc75276129cba4d192000a9bf9
Matthias Arzt <arzt@mpi-cbg.de>
2021-01-20 17:32:29 +0100

    Correctly transform anisotropic images.

--- src/main/java/net/imglib2/labkit/bdv/ImgPlusBdvShowable.java
+++ src/main/java/net/imglib2/labkit/bdv/ImgPlusBdvShowable.java
@@ -4,7 +4,6 @@ package net.imglib2.labkit.bdv;
 import bdv.util.AxisOrder;
 import bdv.util.BdvFunctions;
 import bdv.util.BdvOptions;
-import bdv.util.BdvSource;
 import bdv.util.BdvStackSource;
 import net.imagej.ImgPlus;
 import net.imagej.axis.Axes;
@@ -13,10 +12,6 @@ import net.imglib2.Interval;
 import net.imglib2.realtransform.AffineTransform3D;
 import net.imglib2.type.numeric.NumericType;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -38,17 +33,22 @@ class ImgPlusBdvShowable implements BdvShowable {
 
 	@Override
 	public AffineTransform3D transformation() {
-		return new AffineTransform3D();
+		AffineTransform3D transform = new AffineTransform3D();
+		transform.set(
+			getCalibration(Axes.X), 0, 0, 0,
+			0, getCalibration(Axes.Y), 0, 0,
+			0, 0, getCalibration(Axes.Z), 0);
+		return transform;
 	}
 
 	@Override
 	public BdvStackSource<?> show(String title, BdvOptions options) {
 		String name = image.getName();
-		return BdvFunctions.show(image, name == null ? title : name, options.axisOrder(
-			getAxisOrder()));
+		BdvOptions options1 = options.axisOrder(getAxisOrder()).sourceTransform(transformation());
+		return BdvFunctions.show(image, name == null ? title : name, options1);
 	}
 
-	public AxisOrder getAxisOrder() {
+	private AxisOrder getAxisOrder() {
 		String code = IntStream.range(0, image.numDimensions()).mapToObj(i -> image
 			.axis(i).type().getLabel().substring(0, 1)).collect(Collectors.joining());
 		try {
@@ -57,5 +57,11 @@ class ImgPlusBdvShowable implements BdvShowable {
 		catch (IllegalArgumentException e) {
 			return AxisOrder.DEFAULT;
 		}
+	}
+
+	private double getCalibration(AxisType axisType) {
+		int d = image.dimensionIndex(axisType);
+		if (d == -1) return 1;
+		return image.axis(d).averageScale(image.min(d), image.max(d));
 	}
 }
