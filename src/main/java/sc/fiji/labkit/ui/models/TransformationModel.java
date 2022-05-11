5853054f6fce88a0f287e38993b68e4b747dea21
Matthias Arzt <arzt@mpi-cbg.de>
2022-05-11 12:04:23 +0200

    Add BdvUtils, utility functions for working with BigDataViewer
    Currently has utility functions for blocking any behaviours that allow
    to rotate the view. And a method for resetting the view angle.

--- src/main/java/sc/fiji/labkit/ui/models/TransformationModel.java
+++ src/main/java/sc/fiji/labkit/ui/models/TransformationModel.java
@@ -29,15 +29,12 @@
 
 package sc.fiji.labkit.ui.models;
 
-import bdv.viewer.Source;
 import bdv.viewer.ViewerPanel;
-import net.imglib2.FinalInterval;
 import net.imglib2.Interval;
 import net.imglib2.realtransform.AffineTransform3D;
+import net.imglib2.util.Intervals;
 import sc.fiji.labkit.pixel_classification.RevampUtils;
-
-import java.util.Arrays;
-import java.util.Collections;
+import sc.fiji.labkit.ui.utils.BdvUtils;
 
 /**
  * Needs to be refactored, this is a strange way to enable the "reset view" and
@@ -68,67 +65,8 @@ public class TransformationModel {
 			if (viewerPanel != null) viewerPanel.setTimepoint((int) meanTimePoint);
 			interval = RevampUtils.removeLastDimension(interval);
 		}
-		extracted(0.5, viewerPanel, interval, transformation);
-	}
-
-	/**
-	 * Change BigDataViewer's viewer transform such that the view is aligned with
-	 * the XY plane, the image (source 0) is fitted into the screen, and the center
-	 * pixel of the image is visible.
-	 */
-	public static void resetView(ViewerPanel viewerPanel) {
-		Source<?> spimSource = viewerPanel.state().getSources().get(0).getSpimSource();
-		AffineTransform3D transform = new AffineTransform3D();
-		int timePoint = viewerPanel.state().getCurrentTimepoint();
-		spimSource.getSourceTransform(timePoint, 0, transform);
-		Interval interval = new FinalInterval(spimSource.getSource(timePoint, 0));
-		extracted(1.0, viewerPanel, interval, transform);
-	}
-
-	private static void extracted(double scaleFactor, ViewerPanel viewerPanel, Interval interval,
-		AffineTransform3D transformation)
-	{
-		final double[] screenSize = { viewerPanel.getWidth(), viewerPanel.getHeight() };
-		AffineTransform3D concat = new AffineTransform3D();
-		concat.set(getTransformation(scaleFactor, interval, screenSize));
-		concat.concatenate(transformation.inverse());
-		viewerPanel.state().setViewerTransform(concat);
-	}
-
-	private static AffineTransform3D getTransformation(double scaleFactor, Interval interval,
-		double[] screenSize)
-	{
-		final double scale = scaleFactor * getBiggestScaleFactor(screenSize, interval);
-		final double[] translate = getTranslation(screenSize, interval, scale);
-		final AffineTransform3D transform = new AffineTransform3D();
-		transform.scale(scale);
-		transform.translate(translate);
-		return transform;
-	}
-
-	private static double[] getTranslation(final double[] screenSize,
-		final Interval labelBox, final double labelScale)
-	{
-		final double[] translate = new double[3];
-		for (int i = 0; i < Math.min(translate.length, labelBox
-			.numDimensions()); i++)
-		{
-			translate[i] = -(labelBox.min(i) + labelBox.max(i)) * labelScale / 2;
-			if (i < 2) {
-				translate[i] += screenSize[i] / 2;
-			}
-		}
-		return translate;
-	}
-
-	private static double getBiggestScaleFactor(final double[] screenSize,
-		final Interval labelBox)
-	{
-		final Double[] scales = new Double[2];
-		final double minLength = 20.0;
-		for (int i = 0; i < 2; i++)
-			scales[i] = screenSize[i] / Math.max(labelBox.max(i) - labelBox.min(i),
-				minLength);
-		return Collections.min(Arrays.asList(scales));
+		interval = Intervals.expand(interval, Math.min(interval.dimension(0), 20), 0);
+		interval = Intervals.expand(interval, Math.min(interval.dimension(1), 20), 1);
+		BdvUtils.resetView(viewerPanel, interval, transformation);
 	}
 }
