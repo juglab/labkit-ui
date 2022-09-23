3ed667c63d9e9821b216957e0ecb78998ebbc754
Matthias Arzt <arzt@mpi-cbg.de>
2022-09-23 15:38:58 +0200

    Fix "train classifier" action, when no classifier is in the list

--- src/main/java/sc/fiji/labkit/ui/segmentation/TrainClassifier.java
+++ src/main/java/sc/fiji/labkit/ui/segmentation/TrainClassifier.java
@@ -37,14 +37,13 @@ import sc.fiji.labkit.ui.labeling.Labeling;
 import sc.fiji.labkit.ui.models.SegmentationItem;
 import sc.fiji.labkit.ui.models.SegmenterListModel;
 import sc.fiji.labkit.ui.panel.GuiUtils;
-import sc.fiji.labkit.ui.utils.ParallelUtils;
+import sc.fiji.labkit.ui.segmentation.weka.PixelClassificationPlugin;
 import sc.fiji.labkit.ui.utils.progress.SwingProgressWriter;
 import net.imglib2.util.Pair;
 
 import javax.swing.*;
 import java.util.List;
 import java.util.concurrent.CancellationException;
-import java.util.function.Consumer;
 
 /**
  * Implements the train classifier, and remove classifier menu items.
@@ -55,26 +54,42 @@ public class TrainClassifier {
 
 	public TrainClassifier(Extensible extensible, SegmenterListModel model) {
 		this.model = model;
-		extensible.addMenuItem(MenuBar.SEGMENTER_MENU, "Train Classifier", 1,
-			ignore -> trainSelectedSegmenter(model), null, "ctrl shift T");
-		Consumer<SegmentationItem> train = item -> ParallelUtils.runInOtherThread(
-			() -> {
-				model.selectedSegmenter().set(item);
-				trainSegmenter(model, item);
-			});
 		extensible.addMenuItem(SegmentationItem.SEGMENTER_MENU, "Train Classifier",
-			1, train, GuiUtils.loadIcon("run.png"), null);
+			1, this::onTrainClicked,
+			GuiUtils.loadIcon("run.png"), "ctrl shift T");
 		extensible.addMenuItem(SegmentationItem.SEGMENTER_MENU, "Remove Classifier",
 			3, model::remove,
 			GuiUtils.loadIcon("remove.png"), null);
 	}
 
-	public static void trainSelectedSegmenter(SegmenterListModel model) {
-		trainSegmenter(model, model.selectedSegmenter().get());
+	private void onTrainClicked(SegmentationItem item) {
+		if (item != null)
+			model.selectedSegmenter().set(item);
+		trainSelectedSegmenter();
 	}
 
-	private static void trainSegmenter(SegmenterListModel model, SegmentationItem item) {
-		train(model.trainingData().get(), item);
+	public void trainSelectedSegmenter() {
+		if (model.selectedSegmenter().get() == null)
+			selectSomeSegmenter();
+		trainSelectedSegmenter(model);
+	}
+
+	private void selectSomeSegmenter() {
+		List<SegmentationItem> list = model.segmenters().get();
+		SegmentationItem segmentationItem = list.isEmpty()
+			? addNewSegmenter()
+			: list.get(list.size() - 1);
+		model.selectedSegmenter().set(segmentationItem);
+	}
+
+	private SegmentationItem addNewSegmenter() {
+		PixelClassificationPlugin plugin = new PixelClassificationPlugin();
+		model.context().inject(plugin);
+		return model.addSegmenter(plugin);
+	}
+
+	public static void trainSelectedSegmenter(SegmenterListModel model) {
+		train(model.trainingData().get(), model.selectedSegmenter().get());
 	}
 
 	private static void train(List<Pair<ImgPlus<?>, Labeling>> trainingData, SegmentationItem item) {
